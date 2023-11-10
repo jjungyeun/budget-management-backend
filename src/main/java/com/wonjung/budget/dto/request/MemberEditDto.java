a52c879a4829cf7c88fecc20eb78f7321cb9a78e
jjungyeun <wjyddd@naver.com>
2023-11-10 19:04:28 +0900

    fix #3 - 사용자 정보 수정 요청 DTO 스네이크 케이스 적용

--- src/main/java/com/wonjung/budget/dto/request/MemberEditDto.java
+++ src/main/java/com/wonjung/budget/dto/request/MemberEditDto.java
@@ -1,7 +1,10 @@
 package com.wonjung.budget.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MemberEditDto(
         @NotBlank(message = "닉네임은 필수값입니다.")
         String nickname,
