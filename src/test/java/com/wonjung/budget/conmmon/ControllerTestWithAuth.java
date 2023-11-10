e7c1d1dddda0ae386748868ac3b6f90ed277a083
jjungyeun <wjyddd@naver.com>
2023-11-10 18:30:04 +0900

    test #3 - 컨트롤러 테스트 설정 클래스 구현
    - 컨트롤러 테스트에서 공통적으로 사용하는 설정을 포함하는 클래스 구현
    - 인증 토큰이 필요한 테스트 시 사용할 클래스 구현
    - 테스트 설정파일에 mysql 관련 변수 추가

---
+++ src/test/java/com/wonjung/budget/conmmon/ControllerTestWithAuth.java
@@ -0,0 +1,50 @@
+package com.wonjung.budget.conmmon;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.wonjung.budget.dto.request.LoginDto;
+import com.wonjung.budget.entity.Member;
+import com.wonjung.budget.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+public class ControllerTestWithAuth extends ControllerTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    protected Long memberId;
+    protected String accessToken;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Member member = memberRepository.save(Member.builder()
+                .account("member_for_test_1")
+                .password("$2a$10$MrzeO291MIulxgrV0N81duNdI8sKKucGqcLvTcbSQTnjgy/CCZGbK")
+                .nickname("테스트용 사용자")
+                .pushOption(true)
+                .build());
+        memberId = member.getId();
+
+        LoginDto loginDto = new LoginDto("member_for_test_1", "test123456!");
+        MvcResult result = mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        accessToken = JsonPath.parse(response).read("$.access_token");
+    }
+}
