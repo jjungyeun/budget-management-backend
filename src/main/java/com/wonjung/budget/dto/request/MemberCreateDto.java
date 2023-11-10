1988d7b86ce97b6819c04891a151a715aeffe780
jjungyeun <wjyddd@naver.com>
2023-11-10 18:28:18 +0900

    fix #3 - API 요청/응답 JSON 필드를 스네이크 케이스로 변경하는 설정 추가

--- src/main/java/com/wonjung/budget/dto/request/MemberCreateDto.java
+++ src/main/java/com/wonjung/budget/dto/request/MemberCreateDto.java
@@ -1,7 +1,14 @@
 package com.wonjung.budget.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Pattern;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MemberCreateDto(
         String account,
+        @Pattern(regexp = "^(?=(.*\\d.*))(?=(.*[a-zA-Z].*))(?=(.*[!@#$%^&*()].*)).{10,}$",
+                message = "비밀번호는 숫자, 문자, 특수문자 중 2가지 이상 포함하고, 최소 10자 이상이어야 합니다.")
         String password,
         String nickname,
         Boolean pushOption
