dbcf92c9b799de83b849477b750a3ef4fc04f3d7
jjungyeun <wjyddd@naver.com>
2023-11-10 18:28:57 +0900

    feat #3 - 사용자 정보 조회 API 구현
    - 인증 토큰으로 얻은 사용자 정보를 반환하는 API 구현

--- src/main/java/com/wonjung/budget/controller/MemberController.java
+++ src/main/java/com/wonjung/budget/controller/MemberController.java
@@ -1,13 +1,14 @@
 package com.wonjung.budget.controller;
 
 import com.wonjung.budget.dto.request.MemberCreateDto;
+import com.wonjung.budget.dto.response.MemberDetailDto;
+import com.wonjung.budget.entity.Member;
+import com.wonjung.budget.security.AuthenticationPrincipal;
 import com.wonjung.budget.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/members")
@@ -18,9 +19,18 @@ public class MemberController {
 
     @PostMapping
     public ResponseEntity<Long> signup(
-            @RequestBody MemberCreateDto createDto
+            @Valid @RequestBody MemberCreateDto createDto
     ) {
         Long createdId = memberService.signup(createDto);
         return ResponseEntity.ok(createdId);
+    }
+
+    @GetMapping
+    public ResponseEntity<MemberDetailDto> getMemberDetail(
+            @AuthenticationPrincipal Member member
+    ) {
+        return ResponseEntity.ok(
+                new MemberDetailDto(member.getAccount(), member.getNickname(), member.getPushOption()
+        ));
     }
 }
