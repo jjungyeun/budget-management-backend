48028ece76b334282a4105b8a4200f5209e5d76c
jjungyeun <wjyddd@naver.com>
2023-11-10 18:59:05 +0900

    feat #3 - 요청 DTO 필수값 검증 어노테이션 추가
    - 요청 DTO에서 필수로 받아야하는 필드에 @NotBlank, @NotNull 등 어노테이션 추가
    - 잘못된 요청에 대한 예외 응답 메시지 형식 수정

--- src/main/java/com/wonjung/budget/dto/response/MemberDetailDto.java
+++ src/main/java/com/wonjung/budget/dto/response/MemberDetailDto.java
@@ -2,7 +2,9 @@ package com.wonjung.budget.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
 
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MemberDetailDto(
         String account,
