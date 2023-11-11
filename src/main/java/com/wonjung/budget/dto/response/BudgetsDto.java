75f449357548c7a3e557944e9b6654c354bc6115
jjungyeun <wjyddd@naver.com>
2023-11-12 02:01:59 +0900

    feat #5 - 예산 설정 요청 DTO 검증 어노테이션 추가
    - 예산 정보를 필수로 받도록 @NotEmpty 추가
    - 스네이크 케이스 변환 적용

--- src/main/java/com/wonjung/budget/dto/response/BudgetsDto.java
+++ src/main/java/com/wonjung/budget/dto/response/BudgetsDto.java
@@ -1,10 +1,14 @@
 package com.wonjung.budget.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import java.util.List;
 
 public record BudgetsDto(
         List<BudgetDto> budgets
 ) {
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     public record BudgetDto(
           Long categoryId,
           String categoryName,
